c1cc360928e449de5b67bbde25d2cfb754e7a502
Ivan Vasilev <vanokvas@gmail.com>
2021-04-11 13:06:26 +0300

    Add types for date range and api response

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -8,3 +8,17 @@ interface LicenseUsageDataSummary {
   webstorm: LicenseUsageData[];
   goland: LicenseUsageData[];
 }
+
+interface DateRange {
+  from: import('dayjs').Dayjs;
+  to: import('dayjs').Dayjs;
+}
+
+interface DateStringRange {
+  from: string;
+  to: string;
+}
+
+interface APIResponse extends LicenseUsageDataSummary {
+  dateRange: DateStringRange;
+}
