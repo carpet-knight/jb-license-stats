bcfc7d86d8204154b24b7c47e13f25bcc60eefb9
Ivan Vasilev <vanokvas@gmail.com>
2021-04-12 03:10:10 +0300

    Final version with comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import axios from 'axios';
 import useSWR from 'swr';
 import quarterSupport from 'dayjs/plugin/quarterOfYear';
-import { Alert, Spin } from 'antd';
-import { useMediaQuery } from 'react-responsive';
+import { Spin, notification } from 'antd';
 
 // types for date picker change handler
 import { RangeValue } from 'rc-picker/lib/interface';
@@ -12,6 +11,7 @@ import { RangeInfo } from 'rc-picker/lib/RangePicker';
 
 import { LicenseUsageChart, LicenseUsageTable, DatePicker } from './components';
 import { getStatsWithinRange, mergeStatsWithSameDay } from './utils/statsUtil';
+import { useAdaptiveSize } from './hooks';
 
 import './App.scss';
 
@@ -21,30 +21,26 @@ dayjs.extend(quarterSupport);
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 function App() {
-  // const ErrorMessage: React.FC<{ error: boolean }> = ({ error }) => {
-  //   return error ? (
-  //     <Alert
-  //       style={{ width: '50%' }}
-  //       message="Omg"
-  //       description="Failed to fetch"
-  //       // banner
-  //       type="error"
-  //       closable
-  //       showIcon
-  //     />
-  //   ) : null;
-  // };
+  // used for size adjustment according to screen width
+  const { size } = useAdaptiveSize({
+    smallScreenQuery: '(min-width: 320px)',
+    midScreenQuery: '(min-width: 425px)',
+    largeScreenQuery: '(min-width: 768px)',
+  });
 
-  // const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
-  // const isTablet = useMediaQuery({ query: '(max-width: 768px)' });
-  // const isMobile = useMediaQuery({ query: '(min-width: 768px)' });
-
+  // date range for filtering license usage info
   const [dateRange, setDateRange] = useState<DateRange>();
+
+  // min and max dates (used for specifying allowed date range)
   const [dateLimit, setDateLimit] = useState<DateRange>();
 
   const { data, error } = useSWR<APIResponse>('/api/stats', fetcher);
-  const [filteredData, setFilteredData] = useState<LicenseUsageDataSummary>();
+
+  // data after merge of license usage info with duplicated timestamps
   const [processedData, setProcessedData] = useState<LicenseUsageDataSummary>();
+
+  // processed data within date range
+  const [filteredData, setFilteredData] = useState<LicenseUsageDataSummary>();
 
   useEffect(() => {
     if (!data) return;
@@ -56,7 +52,7 @@ function App() {
       to: dayjs(data.dateRange.to).utc(),
     });
 
-    // default time range (1 quarter)
+    // set default time range (1 quarter)
     setDateRange({
       from: dateStart,
       to: dateStart.add(1, 'quarter').subtract(1, 'day'),
@@ -69,6 +65,7 @@ function App() {
     });
   }, [data]);
 
+  // filter license usage data according to dateRange state
   useEffect(() => {
     if (!dateRange) return;
 
@@ -82,6 +79,21 @@ function App() {
     });
   }, [dateRange, processedData]);
 
+  // display error message on error
+  useEffect(() => {
+    if (error) {
+      notification.error({
+        message: 'Failed To Fetch Data',
+        description: 'Something went terribly wrong.',
+      });
+
+      notification.info({
+        message: 'Trying to fetch data again...',
+      });
+    }
+  }, [error]);
+
+  // change dateRange state according to selected values
   const calendarChangeHandler = (
     ...params: [
       values: RangeValue<dayjs.Dayjs>,
@@ -116,16 +128,17 @@ function App() {
         <div className="container">
           <h1 className="license-usage-stats__title">License Usage Statistics</h1>
           {!data ? (
+            /**************************** Loading Spinner **************************/
             <div className="loading">
               <Spin size="large" tip="Loading license usage statistics..." />
             </div>
           ) : (
             <div className="license-usage-stats__inner">
               <h2>Date Range</h2>
+              {/************************ Date Range Selector *****************************/}
               <DatePicker.RangePicker
                 size="large"
                 allowClear={false}
-                style={{ width: '40%' }}
                 format={['DD MMM, YYYY', 'DD-MM-YYYY', 'DD-MM-YY']}
                 value={(dateRange && [dateRange.from, dateRange.to]) || null}
                 disabledDate={
@@ -149,7 +162,12 @@ function App() {
                 }
                 onCalendarChange={calendarChangeHandler}
               />
-              <LicenseUsageTable data={filteredData} size="small" />
+              {/* ***************** End Date Range Selector ****************** */}
+
+              {/****************** License Usage Table *************************/}
+              <LicenseUsageTable data={filteredData} size={size} />
+
+              {/********************  License Usage Charts *********************/}
               <div className="license-usage-charts">
                 <LicenseUsageChart
                   data={filteredData?.idea}
@@ -163,6 +181,7 @@ function App() {
                   fill="#8884d8"
                 />
               </div>
+              {/******************** End License Usage Charts *******************/}
             </div>
           )}
         </div>
